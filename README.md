404540d46698c69b6621e7016a4740b6c7e89ba8
Dusko Jordanovski <jordanovskid@gmail.com>
2014-07-07 18:25:01 +0200

    Arrays are loaded in parallel, arguments are loaded consecutively

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-microload
-=========
+# Microload
 
-A micro async script loader.
+Microload is a very small asynchronous script and stylesheet loader.  
+
+```
+microload('dependency_1.js', ['dependee_1.js', 'dependee_2.js', 'mystyles.css'], function(){
+  console.log("Scripts Loaded");
+});
+```
+
+The microload function accepts *strings*, *arrays of strings* and an optional *function* as the last argument.
+
+Each string will be treated as a URL to a script or a stylesheet, depending on the extension. Each URL will be loaded consecutively. Microload won't attempt to load the **second** argument until the **first** argument is already loaded.
+
+Microload also accpets arrays of URLs as arguments. These work just the same, except that all the URLs in the array will be loaded in parallel. In the next example we are loading jQuery and underscore in parallel (since they don't depend on other librares) and only after they are both loaded, we load our application scripts.
+
+```
+microload(['jquery.js', 'underscore.js'], ['application-1.js', 'application-2.js'], 'application-1-dependee.js');
+```
+
+Finally, if the last argument is a function, microload will execute it once all resources are succesfully loaded.
+
+```
+microload(['jquery.js', 'underscore.js'], 'application.js', 'application-dependee.js', function(){
+  // Run something once everything is loaded
+  application.init();
+});
+```
+
+**DO NOT** do the following:
+
+```
+microload('script-1.js', function(){
+  // This will wait for script-1.js to load
+});
+microload('script-1.js', function(){
+  // This will be executed BEFORE script-1.js loads
+});
+```
+
+## License
+
+MIT
--- microload.js
+++ microload.js
@@ -1,47 +1,25 @@
 /**
  * @preserve
  * Micro async script loader.
- * Puts a function named `microload` in the window namespace.
- * Usage:
-
-     microload('/script_1.js', 'script_2.js', ['dependancy_1.js', 'dependee_1.js'], function(){
-       // Do something once done.
-     });
- 
- * The `microload` function accepts strings and arrays of strings. The last argument can be a callback.
- * Strings are loaded in parallel. Arrays of strings are loaded in succession (these are good for dependencies).
- * 
- * The loader also protects you from loading the same script twice, for example like this:
-     
-     microload("/script.js", "/another_script.js");
-     microload("/script.js", "/yet_another_script.js");
- *
+ * https://github.com/skid/microload
 **/
 (function (){
-  var loading = {}, loaded  = {};
+  var loaded = {};
 
   function load(src, fn){
-    var script;
-    
-    if(src in loaded){
-      // If the src is already loaded we need to defer the execution to allow the "loaded" counter  
-      // to increment. Otherwise we'll execute the callback once for each already loaded src.
-      return setTimeout(fn, 0);
-    }
-    
-    if(src in loading) {
-      // If we're waiting for the src to load, just push the callback in the queue.
-      // All the callbacks in the queue will be executed at once when it loads.
-      return loading[src].push(fn);
-    }
+    var script, ext = src.substr(src.lastIndexOf('.')).split(/\?|\#/)[0];
 
-    if( src.substr(-4) === '.css' ) {
+    if(src in loaded){
+      return fn();
+    }
+    
+    if(ext === '.css') {
       script = document.createElement('link');
       script.rel = "stylesheet";
       script.async = true;
       script.href = src;
     }
-    else {
+    else if(ext === '.js') {
       script = document.createElement('script');
       script.type = 'text/javascript';
       script.async = true;
@@ -49,61 +27,45 @@
     }
 
     script.onload = script.onreadystatechange = function() {
-      var callbacks = loading[src], fn;
-      delete loading[src];
-
       if (!script.readyState || (script.readyState === 'complete' || script.readyState === 'loaded')) {
         script.onload = script.onreadystatechange = null;
         loaded[src] = (new Date).getTime();
-
-        while(fn = callbacks.shift()) {
-          fn();
-        }
-        return;
+        fn();
       }
-
-      // The ready state changed to something else. This is an error.
-      console && console.log("Microload encountered an error.");
+      else {
+        // The ready state changed to something else. This is an error.
+        throw new Error("Unable to load " + src);
+      }
     };
 
-    loading[src] = [fn];
     document.head.appendChild(script);
   }
 
   window.microload = function(){
-    var arg, i=0, callback = arguments[arguments.length - 1];
+    var arg, args, counter, i = 0;
 
-    (typeof callback !== 'function') && (callback = {});
-    (callback.loading === undefined) && (callback.loading = 0);
-
-    function done(){
-      --callback.loading === 0 && typeof callback === 'function' && callback();
+    args = Array.prototype.slice.call(arguments);
+    arg  = args.shift();
+    
+    // If it's a function, everything was loaded, so execute the callback
+    if('function' === typeof arg) {
+      return arg();
     }
-
-    while(arg = arguments[i++]){
-      // Strings are loaded asynchronously
-      if(typeof arg === 'string') {
-        callback.loading++;
-        load(arg, done);
-      }
-
-      // Arrays of strings are loaded in succession because they depend on each other
-      else if(typeof arg === 'object' && typeof arg.pop === 'function') {
-        (function(arg){
-          var next = arg.shift();
-          if(!next) {
-            return done();
-          }
-
-          ++callback.loading;
-          load(next, function(){
-            if( arg.length ) {
-              // Decrement loading since we're not calling done() yet
-              --callback.loading;
-            }
-            microload(arg, callback);
-          });
-        })(arg.slice());
+    
+    // It's a string, load consecutively
+    else if('string' === typeof arg){
+      load(arg, function(){
+        microload.apply(window, args);
+      });
+    }
+    
+    // It's an array, load in parallel
+    else if('[object Array]' === Object.prototype.toString.call(arg)){
+      counter = arg.length;
+      while(url = arg[i++]){
+        load(url, function(){
+          --counter === 0 && microload.apply(window, args);
+        });
       }
     }
   }
